a1a9e0a65b1dc1b4d60678296cc3476c3976132a
Aymeric Ingargiola <aymericfbm@gmail.com>
2022-05-31 18:19:47 +0200

    add egs free games cron

---
+++ crons/egs.js
@@ -0,0 +1,52 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const CronJob = require('cron').CronJob;
+const {COMMON, client, EgsFreeGamesDb} = require("../index");
+const { getChannelById } = require('../helpers/getChannelsInfos');
+const tools = require('../tools/tools');
+const egsUrl = "https://store.epicgames.com";
+module.exports = {
+    async getFreeGames() {
+        var job = new CronJob(
+            '0 0 20 */ * *',
+            async function() {
+                console.log("Checking new Epic Games Store free games...")
+                let freeGames = [];
+                try {
+                    const egsFreeGame = await fetch("https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=fr&country=FR&allowCountries=FR");
+                    const response = egsFreeGame.body._outBuffer.toString();
+                    const formatResponse = response.substr(0, response.lastIndexOf("{}}") + 3);
+                    const json = JSON.parse(formatResponse);
+                    freeGames = json.data.Catalog.searchStore.elements.filter(g => g.title != "Mystery Game");
+                } catch (err) {
+                    console.log(`Error while checking Epic Games Store free games !`, err)
+                    return
+                }
+                await tools.asyncForEach(freeGames, async (game) => {
+                    const id = game.id;
+                    const isFreeGameExist = await EgsFreeGamesDb.findOne({ where: { id: id } });
+                    if (isFreeGameExist) return
+                    const title = game.title;
+                    const offerType = game.offerType.toLowerCase();
+                    const productSlug = game.productSlug;
+                    const url = `${egsUrl}/${offerType === "bundle" ? "bundles" : offerType === "game" ? "games" : "applications"}/${productSlug}`;
+                    const startDate = game.promotions.promotionalOffers[0].promotionalOffers[0].startDate;
+                    const endDate = game.promotions.promotionalOffers[0].promotionalOffers[0].endDate;
+                    console.log(`${title}[${id}] is a new free game !`)
+                    await EgsFreeGamesDb.create({
+                        id: id,
+                        title: title,
+                        startDate: startDate,
+                        endDate: endDate
+                    });
+                    console.log(`${title}[${id}] saved to database.`)
+                    const channel = await getChannelById(COMMON.channels['🎮videogames'].id)
+                    channel.send(`Nouveau jeu Epic Games Store gratuit ! ${title} : ${url}`)
+                })
+            },
+            null,
+            false,
+            'Europe/Paris'
+        )
+        job.start()
+    }
+};
--- index.js
+++ index.js
@@ -28,15 +28,27 @@ const MessagesDb = sequelize.define('messages', {
 	content: DataTypes.TEXT
 });
 
+const EgsFreeGamesDb = sequelize.define('egsFreeGamesDb', {
+	id: {
+		type: DataTypes.STRING,
+		primaryKey: true
+	},
+    title: DataTypes.STRING,
+	startDate: DataTypes.STRING,
+	endDate: DataTypes.STRING
+});
+
 client.commands = new Collection();
 
 // When the client is ready, run this code (only once)
 client.once('ready', async () => {
     await MessagesDb.sync();
+	await EgsFreeGamesDb.sync();
 	module.exports.COMMON = COMMON;
 	module.exports.client = client;
 	module.exports.guild = client.guilds.cache.get(guildId);
 	module.exports.MessagesDb = MessagesDb;
+	module.exports.EgsFreeGamesDb = EgsFreeGamesDb;
 
 	const commandsPath = path.join(__dirname, 'commands');
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -66,6 +78,9 @@ client.once('ready', async () => {
 
 	const { writePostOfTheWeek } = require('./crons/postsOfTheWeek');
 	writePostOfTheWeek()
+
+	const { getFreeGames } = require('./crons/egs');
+	getFreeGames()
 });
 
 client.on('interactionCreate', async interaction => {
